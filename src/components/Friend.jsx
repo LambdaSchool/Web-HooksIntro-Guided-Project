da45a5e9165672d30f0fa850edb2aea840a17229
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-18 15:19:52 +0100

    wip

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const style = {
+  border: '1px solid black',
+  padding: '10px',
+  margin: '10px',
+};
+
 // fake request to server
 export function checkOnlineStatus(id) {
   console.log('starting online status check');
@@ -19,8 +25,8 @@ export class FriendTraditional extends React.Component {
     const { friend } = this.props;
 
     return (
-      <div>
-        <h3>{friend.name}</h3>
+      <div style={style}>
+        <h3>(traditional friend) {friend.name}</h3>
 
         <div style={{ color: isOnline ? 'green' : 'red' }}>
           is {!isOnline && 'NOT '}online
@@ -35,8 +41,8 @@ export function FriendModern({ friend }) {
   const isOnline = false;
 
   return (
-    <div>
-      <h3>{friend.name}</h3>
+    <div style={style}>
+      <h3>(modern friend) {friend.name}</h3>
 
       <div style={{ color: isOnline ? 'green' : 'red' }}>
         is {!isOnline && 'NOT '}online
